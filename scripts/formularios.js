e10c6acdda25e3105af1d989448d7593fad82dd7
Mariaa-Isabel <maria.isa.amacias@gmail.com>
2024-11-07 21:03:04 -0500

    registrar usuarios e iniciar sesion, estilos formularios

--- scripts/formularios.js
+++ scripts/formularios.js
@@ -25,5 +25,56 @@ function activarAlternarFormularios() {
   });
 }
 
+function extraerDatosFormulario(form) {
+  return Array.from(form.elements).reduce((acc, elemento) => {
+    if (elemento.type !== "submit" && elemento.type !== "button") {
+      acc[elemento.name] = elemento.value;
+    }
+    return acc;
+  }, {});
+}
+
+function registrarUsuario(e) {
+  e.preventDefault();
+  const form = e.target;
+  const email = document.getElementById("register-email").value;
+  const data = extraerDatosFormulario(form);
+  localStorage.setItem(email, JSON.stringify(data));
+  mostrarForm('#ingreso');
+}
+
+function iniciarSesion(e) {
+  e.preventDefault();
+  const form = e.target;
+  const datosForm = extraerDatosFormulario(form);
+  const usuario = localStorage.getItem(datosForm.email);
+  if (!usuario) {
+    alert("Correo o contraseña incorrecta");
+    return;
+  }
+
+  try {
+    const datosUsuario = JSON.parse(usuario);
+    if (
+      datosUsuario.email != datosForm.email ||
+      datosUsuario.password != datosForm.password
+    ) {
+      alert("Correo o contraseña incorrecta");
+    } else {
+      localStorage.setItem('usuarioActual', datosUsuario.email);
+      location.href = '/index.html';
+    }
+  } catch (e) {
+    alert("Correo o contraseña incorrecta");
+  }
+}
+
+function activarSubmitFormularios() {
+  document
+    .getElementById("registro")
+    .addEventListener("submit", registrarUsuario);
+  document.getElementById("ingreso").addEventListener("submit", iniciarSesion);
+}
+
 mostrarForm(document.location.hash);
 activarAlternarFormularios();
